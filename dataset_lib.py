ecbbcc091a881ad42e9cd8d399a4e5a61c6abbbc
Suhren <adamsg93@hotmail.com>
2019-01-03 17:29:50 +0100

    CMD update
    - Added functionality to load, save, input, train and test using the command interface

--- dataset_lib.py
+++ dataset_lib.py
@@ -1,10 +1,21 @@
 import utils
 import numpy as np
 
-class DataEntry():
+class DataEntry(object):
     def __init__(self, inp, exp):
         self.inp = inp
         self.exp = exp
+
+class DataSet(object):
+    entries = []
+
+    def __init__(self, name, nInputs, nOutputs):
+        self.name = name
+        self.nInputs = nInputs
+        self.nOutputs = nOutputs
+
+    def __str__(self):
+        return '%s: %i -> %i' % (self.name, self.nInputs, self.nOutputs)
 
 #The MLP works better when the pixels are a value between 0.0f and 1.0f as oposed to 0 to 255?
 #Can it be because the expected output is in the range 0.0f to 1.0f?
@@ -20,10 +31,14 @@ def _label_to_array(label):
 _labels_training, _images_training = utils.loadMNIST("mnist//train-labels-idx1-ubyte", "MNIST//train-images-idx3-ubyte")
 _labels_test, _images_test = utils.loadMNIST("mnist//t10k-labels-idx1-ubyte", "MNIST//t10k-images-idx3-ubyte")
 
-set_mnist_training = []
-for i in range(len(_labels_training)):
-    set_mnist_training.append(DataEntry(_image_to_array(_images_training[i]), _label_to_array(_labels_training[i])))
+datasets = []
 
-set_mnist_test = []
+set_mnist_training = DataSet("MNIST training set", 784, 10)
+datasets.append(set_mnist_training)
+for i in range(len(_labels_training)):
+    set_mnist_training.entries.append(DataEntry(_image_to_array(_images_training[i]), _label_to_array(_labels_training[i])))
+
+set_mnist_test = DataSet("MNIST test set", 784, 10)
+datasets.append(set_mnist_test)
 for i in range(len(_labels_test)):
-    set_mnist_test.append(DataEntry(_image_to_array(_images_test[i]), _label_to_array(_labels_test[i])))
+    set_mnist_test.entries.append(DataEntry(_image_to_array(_images_test[i]), _label_to_array(_labels_test[i])))
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 """
 This example uses docopt with the built in cmd module to demonstrate an
-interactive command application.
+interactive command application. Based on https://github.com/docopt/docopt/tree/master/examples.
+
 Usage:
-    my_program tcp <host> <port> [--timeout=<seconds>]
-    my_program serial <port> [--baud=<n>] [--timeout=<seconds>]
+    my_program list (mlp | dat)
+    my_program load (mlp | dat) <index>
+    my_program input <index>
+    my_program train <repeat>
+    my_program test
+    my_program save <filename>
     my_program (-i | --interactive)
     my_program (-h | --help | --version)
+
 Options:
     -i, --interactive  Interactive Mode
     -h, --help  Show this screen and exit.
-    --baud=<n>  Baudrate [default: 9600]
 """
 
 import sys
+import os
 import cmd
 import numpy as np
 import matplotlib.cm as cm
@@ -26,11 +32,10 @@ import utils
 import afun_lib
 import dataset_lib
 
-#http://rogerdudler.github.io/git-guide/
-#https://google.github.io/styleguide/pyguide.html
-#https://github.com/docopt/docopt/blob/master/examples/interactive_example.py
-
 def docopt_cmd(func):
+    #http://rogerdudler.github.io/git-guide/
+    #https://google.github.io/styleguide/pyguide.html
+    #https://github.com/docopt/docopt/blob/master/examples/interactive_example.py
     """
     This decorator is used to simplify the try/except block and pass the result
     of the docopt parsing to the called action.
@@ -64,20 +69,76 @@ class MyInteractive (cmd.Cmd):
     intro = 'Welcome to my interactive program!' \
         + ' (type help for a list of commands.)'
     prompt = '(my_program) '
-    file = None
+    
+    _mlp = None
+    _dat = None
 
     @docopt_cmd
-    def do_tcp(self, arg):
-        """Usage: tcp <host> <port> [--timeout=<seconds>]"""
-        print(arg)
+    def do_list(self, arg):
+        """Usage: list (mlp | dat)"""
+        if arg['mlp']:
+            files = os.listdir('networks/')
+            for i, name in enumerate(files):
+                print('%i %s' % (i, name))
+        elif arg['dat']:
+            for i, s in enumerate(dataset_lib.datasets):
+                print('%i %s' % (i, s))
 
     @docopt_cmd
-    def do_serial(self, arg):
-        """Usage: serial <port> [--baud=<n>] [--timeout=<seconds>]
-    Options:
-        --baud=<n>  Baudrate [default: 9600]
-            """
-        print(arg)
+    def do_load(self, arg):
+        """Usage: load (mlp | dat) <index>"""
+        if arg['mlp']:
+            i = int(arg['<index>'])
+            files = os.listdir('networks/')
+            self._mlp = utils.loadMLP('networks/' + files[i])
+            print('Loaded %s' % (files[i]))
+        elif arg['dat']:
+            i = int(arg['<index>'])
+            self._dat = dataset_lib.datasets[i]
+            print('Loaded %s' % (self._dat))
+            
+    @docopt_cmd
+    def do_input(self, arg):
+        """Usage: input <index>"""
+        if self._mlp == None:
+            print('No MLP specified')
+            return
+        if self._dat == None:
+            print('No dataset specified')
+            return
+        inputEntry(self._mlp, self._dat, int(arg['<index>']))
+        
+    @docopt_cmd
+    def do_train(self, arg):
+        """Usage: train <repeat>"""
+        if self._mlp == None:
+            print('No MLP specified')
+            return
+        if self._dat == None:
+            print('No dataset specified')
+            return
+        train(self._mlp, self._dat, int(arg['<repeat>']))
+
+    @docopt_cmd
+    def do_test(self, arg):
+        """Usage: test"""
+        if self._mlp == None:
+            print('No MLP specified')
+            return
+        if self._dat == None:
+            print('No dataset specified')
+            return
+        test(self._mlp, self._dat)
+
+    @docopt_cmd
+    def do_save(self, arg):
+        """Usage: save <filename>"""
+        if self._mlp == None:
+            print('No MLP specified')
+            return
+        path = 'networks/' + arg['<filename>'] + '.txt'
+        utils.saveMLP(self._mlp, path)
+        print('Saved MLP as %s' % (path))
 
     def do_quit(self, arg):
         """Quits out of Interactive Mode."""
@@ -87,7 +148,7 @@ class MyInteractive (cmd.Cmd):
 
 def maxIndex(x):
     return np.argmax(x)
-    
+
 def showImage(image):
     plt.figure()
     plt.imshow(image, cmap=plt.get_cmap('gray'), vmin=0, vmax=255)
@@ -103,44 +164,9 @@ def main():
 
     print(opt)
 
-    """
-    network = mlp.MLP.from_arguments('MyMLP', 784, (12, 10), 0.01, afun_lib.ELLIOT_SIG)
-
-    while True:
-        command = input('Enter command: ').split()
-        if command[0] == 'train':
-            n = int(command[1]) if (len(command)) > 1 else 1
-            train(network, dataset_lib.set_mnist_training, n)
-        elif command[0] == 'input':
-            inputEntry(network, dataset_lib.set_mnist_training, int(command[1]))
-        elif command[0] == 'save':
-            if network is None:
-                print('No MLP specified')
-                continue
-            if len(command) < 2:
-                print('Specify a name for the file')
-                continue
-            utils.saveMLP(network, 'networks/' + command[1] + '.txt')
-        elif command[0] == 'load':
-            if network is None:
-                print('No MLP specified')
-                continue
-            if len(command) < 2:
-                print('Specify a name for the mlp')
-                continue
-            try:
-                print('Loading %s...' % (command[1]))
-                network = utils.loadMLP('networks/' + command[1] + '.txt')
-                print('Loading done')
-            except:
-                print('Could not load file')
-        else:
-            print('Unknown command')
-        """
-
 def inputEntry(network, dataset, i):
-    print('Input %s:' % (maxIndex(dataset[i].exp)))
-    out = network.eval(dataset[i].inp, dataset[i].exp)
+    print('Input %s:' % (maxIndex(dataset.entries[i].exp)))
+    out = network.eval(dataset.entries[i].inp, dataset.entries[i].exp)
     print('Guess: %i' % maxIndex(out))
     print('Output: %s' % out)
     # print('Cost: %.8f' % (network.getCost()))
@@ -149,11 +175,23 @@ def train(network, dataset, n):
     print('Training %i times...' % (n))
     for i in range(n):
         sum = 0.0
-        for e in dataset:
+        for e in dataset.entries:
             sum += network.train(e.inp, e.exp)
             #print('%i of %i: Cost: %.8f' % (i, len(trainSet), cost))
-        print('Set %i of %i: ave. cost: %.16f' % (i + 1, n, sum / len(dataset)))
+        print('Set %i of %i: ave. cost: %.16f' % (i + 1, n, sum / len(dataset.entries)))
     print('Done training')
-    
+
+def test(network, dataset):
+    total = len(dataset.entries)
+    correct = 0
+    print('Testing...')
+    for e in dataset.entries:
+        exp = maxIndex(e.exp)
+        out = maxIndex(network.eval(e.inp, e.exp))
+        if exp == out:
+            correct += 1
+    percentage = correct * 100.0 / total
+    print('Done testing: Classified %i of %i correct (%.4f%%)' % (correct, total, percentage))
+
 if __name__ == '__main__':
     main()
